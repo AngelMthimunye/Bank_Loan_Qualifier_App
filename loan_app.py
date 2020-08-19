36072f686ac05cabe8e38122c5fbd37e67f190d0
Angel Mthimunye <62521227+AngelMthimunye@users.noreply.github.com>
2020-08-19 09:13:39 +0200

    Add App

--- loan_app.py
+++ loan_app.py
@@ -14,13 +14,18 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
+    status = ""
     
-        
     features = [float(x) for x in request.form.values()]
     final_features = [np.array(features)]
     prediction = model.predict(final_features)
     
-    return render_template("loan.html", prediction_text="STATUS: {}".format(prediction))
+    if prediction == 0:
+        status = "Thank you for your participation. Unfortunately you do not qualify for a Bank Loan."
+    else:
+        status = "Congratulations, you qualify for a bank loan."
+    
+    return render_template("loan.html", prediction_text="STATUS: {}".format(status))
 
 if __name__ == "__main__":
     app.run(debug=True)
